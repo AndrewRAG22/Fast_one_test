723bf83e83b63846103bdc0c59ec0c44992f2aec
Andrew <anjojusticeiroo@gmail.com>
2026-05-19 17:13:06 -0300

    Iniciando nessa conta

--- fast_one/app.py
+++ fast_one/app.py
@@ -3,9 +3,11 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_one.schemas import Message
+from fast_one.schemas import Message, UserDB, UserPublic, UserSchema, UserList
 
 app = FastAPI(title='Minha Api')  # nome da minha api
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -24,3 +26,22 @@ def exercicio_aula_02():
         <h1> Olá Mundo </h1>
       </body>
     </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        # descompacta o objeto user e passa os campos para o UserDB
+        **user.model_dump(),
+        id=len(database) + 1,
+    )
+    database.append(user_with_id)
+    return user_with_id
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+@app.put('/user/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def update_user(user: UserSchema, user_id: int):
+    breakpoint()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,8 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_one.app import app
-
-
-def test_root_deve_retornar_hello_world():
+def test_root_deve_retornar_hello_world(client):
     # Arrange = organizar
-    client = TestClient(app)
     # Act = agir
     response = client.get('/')
     # Assert = afirmar
@@ -15,10 +10,40 @@ def test_root_deve_retornar_hello_world():
     assert response.status_code == HTTPStatus.OK
 
 
-def test_exercicio_ola_mundo_em_html():
-    client = TestClient(app)
-
+def test_exercicio_ola_mundo_em_html(client):
     response = client.get('/exercicio-html')
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1> Olá Mundo </h1>' in response.text
+
+
+def test_criar_usuario(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'andrew',
+            'email': 'andrew@example.com',
+            'password': 'andrew13',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'andrew@example.com',
+        'username': 'andrew',
+    }
+
+def test_listar_usuarios(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'email': 'andrew@example.com',
+                'username': 'andrew',
+            },
+        ]
+    }
